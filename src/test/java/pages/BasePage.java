01c47f6b352f2a4624852f96ae76683c75e3bfcc
raguram91 <raguramselvaraj@gmail.com>
2023-04-11 22:48:10 +0530

    back to original

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -11,7 +11,6 @@ import java.time.Duration;
 public class BasePage {
     public WebDriver driver;
     public WebDriverWait wait;
-    public String codeEdited;
 
     //Constructor
     public BasePage(WebDriver driver) {
